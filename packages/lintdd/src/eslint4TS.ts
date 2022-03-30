9fa27e58fc96ee2f86b9eae499955aa7d16cb4af
Y-lonelY <luckyyoung666@gmail.com>
2022-03-30 14:59:02 +0800

    fix: lintdd parserOptions.project

--- packages/lintdd/src/eslint4TS.ts
+++ packages/lintdd/src/eslint4TS.ts
@@ -4,6 +4,7 @@ import { eslintRules, prettierConflictRules } from './eslintOptions/rules'
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   // 顶级域变量
   env: {
     browser: true,
@@ -32,5 +33,9 @@ module.exports = {
     'import/external-module-folders': ['node_modules', 'node_modules/@types'],
     // Add polyfills to the settings section of your eslint config. Append the name of the object and the property if one exists
     polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+  },
+  parserOptions: {
+    project: './tsconfig.json', // default project config
+    createDefaultProgram: true, // 兼容未在 tsconfig.json 中 provided 的文件
   },
 }
