b4300a81827e16b6ffb20b064dd5ef296ebe3df5
Y-lonelY <luckyyoung666@gmail.com>
2022-05-20 14:58:56 +0800

    chore(joyutils): add control

--- packages/joyutils/scripts/control.mjs
+++ packages/joyutils/scripts/control.mjs
@@ -1,11 +1,26 @@
 #!/usr/bin/env zx
 import { resolve } from 'path'
+import dayjs from 'dayjs'
 
 const log = console.log
 const ALERT_MESSAGE = chalk.red('\n🤔Something went wrong please try again!\n')
-const cmds = ['dev', 'build', 'rls', 'example']
-const versionGrade = ['major', 'minor', 'patch']
+const cmds = ['dev', 'build', 'rls', 'example', 'help']
+const versionLevel = ['major', 'minor', 'patch']
 const [nodePath, zxPath, scriptPath, ...params] = process.argv
+
+const HELP_INFO = `
+${chalk.greenBright(`Hi, thanks for using joyutils with zx~✨\n`)}
+
+${chalk.blueBright(`Commands: `)}
+  dev                 tsc --watch
+  build               tsc
+  rls                 Release to npm registry!
+    --set-version     Set target version.
+  example             Compile the example index.ts and execute it.
+  help                Show some helps!
+
+${chalk.yellowBright(`Nice to code!🎉`)}
+`
 
 // choices: string[]
 let choices
@@ -33,9 +48,19 @@ switch (entry) {
   case 'rls':
     exeRelease(options)
     break
+  case 'help':
+    log(HELP_INFO)
+    break
   default:
+    log(HELP_INFO)
     log(ALERT_MESSAGE)
     process.exit(1)
+}
+
+async function getPackageVersion() {
+  const packageJson = await fs.readFile('./package.json', 'utf-8')
+  const pkg = packageJson && JSON.parse(packageJson)
+  return (pkg && pkg.version) || undefined
 }
 
 async function exeDevelop(options) {
@@ -51,4 +76,62 @@ async function exeExample(options) {
   await exeBuild()
   await $`tsc --project ${examplePath}/tsconfig.example.json`
   await $`node ${examplePath}/index.js`
+}
+
+async function choosePackageVersion() {
+  const versionStr = await getPackageVersion()
+  const versionArr = versionStr.split('.')
+  let targetVersion
+  if (versionStr) {
+    const getVersionGrade = await question(
+      `Current version is ${chalk.cyanBright(
+        versionStr
+      )} ,which level do you want to add?\n`,
+      {
+        choices: versionLevel,
+      }
+    )
+    switch (getVersionGrade) {
+      case 'patch':
+        versionArr[2] = Number(versionArr[2]) + 1
+        targetVersion = versionArr.join('.')
+        break
+      case 'minor':
+        // minor + 1, patch version 置为 0
+        versionArr[1] = Number(versionArr[1]) + 1
+        versionArr[2] = 0
+        targetVersion = versionArr.join('.')
+        break
+      case 'major':
+        // major + 1, patch version 置为 0
+        versionArr[0] = Number(versionArr[0]) + 1
+        versionArr[1] = 0
+        versionArr[2] = 0
+        targetVersion = versionArr.join('.')
+        break
+      default:
+        versionArr[2] = Number(versionArr[2]) + 1
+        targetVersion = versionArr.join('.')
+        break
+    }
+  }
+  return targetVersion
+}
+
+// pnpm run zx rls --set-version 1.0.0
+async function exeRelease(options) {
+  let setVersion
+  const versionIndex = options.findIndex((i) => i === '--set-version')
+  if (versionIndex === -1) {
+    setVersion = await choosePackageVersion()
+  } else {
+    setVersion = options[versionIndex + 1]
+  }
+  await exeBuild()
+  await $`pnpm version ${setVersion}`
+  await $`git add package.json`
+  await $`git commit -m "rls: joyutils ${setVersion} at ${day().format(
+    'YYYY-MM-DD HH:mm:ss'
+  )}"`
+  await $`pnpm publish`
 }
