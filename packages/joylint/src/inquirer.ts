37f9ea3124236b6973e245e34cfaff4c9c59e27b
Y-lonelY <luckyyoung666@gmail.com>
2022-11-24 11:27:13 +0800

    ref: :zap: divide into dependencies

--- packages/joylint/src/inquirer.ts
+++ packages/joylint/src/inquirer.ts
@@ -1,22 +1,7 @@
 import inquirer from 'inquirer'
 import { log, getYarnInfo, getPnpmInfo, getNpmInfo } from './utils'
 import pico from 'picocolors'
-import { npmChoices } from './options'
-
-const choices = [
-  {
-    name: 'Install lint tools, includes: Eslint, Prettier, Stylelint',
-    value: 'lints',
-  },
-  {
-    name: 'Init husky and generate githooks scripts, inclued: commit-msg, pre-commit',
-    value: 'gitprocess',
-  },
-  {
-    name: 'All Above Task',
-    value: 'all',
-  },
-]
+import { COMMAND_CHOICES, NPM_CHOICES, FRAME_CHOICES } from './options'
 
 // Generate node package manager
 async function genNpmChoices(): Promise<{ name: string; value: string }[]> {
@@ -29,30 +14,46 @@ async function genNpmChoices(): Promise<{ name: string; value: string }[]> {
       }
     })
   }
-  return npmChoices.map((name) => ({ name, value: name }))
+  return NPM_CHOICES.map((name) => ({ name, value: name }))
 }
 
 export async function taskProcessor() {
-  const npmToolsChoices = await genNpmChoices()
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'command',
-      message: `What do you want to do next?`,
-      default: 'all',
-      choices: choices,
-    },
-    {
-      type: 'list',
-      name: 'manager',
-      message: `Which package manager to ues?`,
-      default: 'yarn',
-      choices: npmToolsChoices,
-    },
-  ])
-  if (answers) {
+  try {
+    const npmToolsChoices = await genNpmChoices()
+    let answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'command',
+        message: `What do you want to do next?`,
+        default: 'all',
+        choices: COMMAND_CHOICES,
+      },
+      {
+        type: 'list',
+        name: 'manager',
+        message: `Which package manager to ues?`,
+        default: 'yarn',
+        choices: npmToolsChoices,
+      },
+    ])
+    if (['all', 'lints'].includes(answers.command)) {
+      const frame_answer = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'command',
+          message: `Which frame you are using?`,
+          default: 'react',
+          choices: FRAME_CHOICES,
+        },
+      ])
+      answers = {
+        ...answers,
+        ...frame_answer,
+      }
+    }
     return answers
+  } catch (error) {
+    log(pico.red(`Error throw from inquirer!`))
+    process.exit(1)
   }
-  log(pico.red(`Error throw from inquirer!`))
-  process.exit(1)
 }
--- packages/joylint/src/options.ts
+++ packages/joylint/src/options.ts
@@ -1,31 +1,170 @@
-const npmChoices = ['npm', 'yarn', 'pnpm']
+const NPM_CHOICES = ['npm', 'yarn', 'pnpm']
 
-const lintTools = [
+const COMMAND_CHOICES = [
   {
-    name: 'eslint',
-    version: '8.20.0',
+    name: 'Install lint tools, includes: Eslint, Prettier, Stylelint',
+    value: 'lints',
   },
   {
-    name: 'prettier',
-    version: '2.7.1',
+    name: 'Init husky and generate githooks scripts, inclued: commit-msg, pre-commit',
+    value: 'gitprocess',
   },
   {
-    name: 'stylelint',
-    version: '14.9.1',
+    name: 'All Above Task',
+    value: 'all',
   },
 ]
 
-const huskyTools = [
+const FRAME_CHOICES = [
   {
-    name: 'husky',
-    version: 'latest',
+    name: 'React + TypeScript + Prettier + Stylelint',
+    value: 'react',
   },
+  {
+    name: 'Vue2 + Prettier + Stylelint',
+    value: 'vue2',
+  },
+  {
+    name: 'Vuu3 + TypeScript + Prettier + Stylelint',
+    value: 'vue3',
+  },
+]
+
+const LINT_TOOLS = [
+  {
+    name: 'eslint',
+    version: '^8.20.0',
+  },
+  {
+    name: 'prettier',
+    version: '^2.7.1',
+  },
+  {
+    name: 'stylelint',
+    version: '^14.9.1',
+  },
+]
+
+const COMMON_DEPS = [
+  {
+    name: 'eslint-config-prettier',
+    version: '^8.4.0',
+  },
+  {
+    name: 'eslint-plugin-prettier',
+    version: '^4.0.0',
+  },
+  {
+    name: 'eslint-plugin-promise',
+    version: '^6.0.0',
+  },
+  {
+    name: '@babel/eslint-parser',
+    version: '^7.17.0',
+  },
+]
+
+const REACT_DEPS = [
+  ...COMMON_DEPS,
+  {
+    name: '@typescript-eslint/parser',
+    version: '^5.13.0',
+  },
+  {
+    name: '@typescript-eslint/eslint-plugin',
+    version: '^5.13.0',
+  },
+  {
+    name: 'eslint-plugin-react',
+    version: '^7.29.2',
+  },
+  {
+    name: 'eslint-plugin-react-hooks',
+    version: '^4.3.0',
+  },
+  {
+    name: '@babel/preset-env',
+    version: '^7.16.11',
+  },
+  {
+    name: '@babel/preset-react',
+    version: '^7.16.7',
+  },
+  {
+    name: '@babel/preset-typescript',
+    version: '^7.16.7',
+  },
+  {
+    name: '@babel/plugin-proposal-class-properties',
+    version: '^7.16.7',
+  },
+  {
+    name: '@babel/plugin-proposal-decorators',
+    version: '^7.17.2',
+  },
+]
+
+const VUE2_DEPS = [
+  ...COMMON_DEPS,
+  {
+    name: 'vue-eslint-parser',
+    version: '^8.3.0',
+  },
+]
+
+const VUE3_DEPS = [
+  ...COMMON_DEPS,
+  {
+    name: 'vue-eslint-parser',
+    version: '^8.3.0',
+  },
+  {
+    name: '@typescript-eslint/parser',
+    version: '^5.13.0',
+  },
+  {
+    name: 'eslint-plugin-vue',
+    version: '^9.8.0',
+  },
+]
+
+const STYLELINT_DEPS = [
+  {
+    name: 'stylelint-config-css-modules',
+    version: '^3.0.0',
+  },
+  {
+    name: 'stylelint-config-prettier',
+    version: '^9.0.3',
+  },
+  {
+    name: 'stylelint-config-standard',
+    version: '^25.0.0',
+  },
+  {
+    name: 'stylelint-declaration-block-no-ignored-properties',
+    version: '^2.5.0',
+  },
+]
+
+const HUSKY_DEPS = [
   {
     name: 'lint-staged',
     version: 'latest',
   },
 ]
 
-const huskyScriptPath = ['verify_commit_msg.mjs', 'lint_staged.mjs']
+const HUSKY_SCRIPT_PATH = ['verify_commit_msg.mjs', 'lint_staged.mjs']
 
-export { npmChoices, lintTools, huskyTools, huskyScriptPath }
+export {
+  COMMAND_CHOICES,
+  NPM_CHOICES,
+  FRAME_CHOICES,
+  LINT_TOOLS,
+  REACT_DEPS,
+  VUE2_DEPS,
+  VUE3_DEPS,
+  STYLELINT_DEPS,
+  HUSKY_DEPS,
+  HUSKY_SCRIPT_PATH,
+}
