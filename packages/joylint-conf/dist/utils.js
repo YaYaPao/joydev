3ac9461813daa01df90aa598403890baee57c9fc
Y-lonelY <luckyyoung666@gmail.com>
2022-12-16 10:52:36 +0800

    rls: joylint 1.0.5

--- packages/joylint-conf/dist/utils.js
+++ packages/joylint-conf/dist/utils.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isTsProject = void 0;
 const node_fs_1 = require("node:fs");
-const node_path_1 = require("node:path");
+const path = require("node:path");
 // 根据是否含有 tsconfig.json 来判断当前项目是否为 TS 项目
-exports.isTsProject = (0, node_fs_1.existsSync)(node_path_1.default.join(process.cwd() || '.', './tsconfig.json'));
+exports.isTsProject = (0, node_fs_1.existsSync)(path.join(process.cwd() || '.', './tsconfig.json'));
