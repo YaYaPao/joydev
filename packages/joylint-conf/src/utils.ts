722b0d66fc50cb7867f8daab2f02e69bc39158d7
Y-lonelY <luckyyoung666@gmail.com>
2022-12-16 10:51:49 +0800

    fix(conf): :bug: path.join

--- packages/joylint-conf/src/utils.ts
+++ packages/joylint-conf/src/utils.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'node:fs'
-import path from 'node:path'
+import * as path from 'node:path'
 // 根据是否含有 tsconfig.json 来判断当前项目是否为 TS 项目
 export const isTsProject = existsSync(
   path.join(process.cwd() || '.', './tsconfig.json')
