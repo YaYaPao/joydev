7a7757854110f59400da3a1f8ddef7c6fdf80109
Y-lonelY <luckyyoung666@gmail.com>
2023-03-10 16:23:36 +0800

    feat(joylint): update verfiy commit message

--- packages/joylint/public/verify_commit_msg.mjs
+++ packages/joylint/public/verify_commit_msg.mjs
@@ -1,31 +1,51 @@
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 
+function genHelpInfo() {
+  const types = `
+  feat | :sparkles: | :tada: -> add new feature/project
+  docs | :memo: -> add or update documents
+  style | :lipstick: -> add or update the UI and style files
+  fix | :bug: -> fix a bug
+  perf | :zap: -> improve performance
+  ref | :recycle: -> refactor code
+  rm | :fire: | :coffin: -> remove code or files
+  break | :boom: -> introduce breaking changes
+  conf | :wrench: | :hammer: -> add or update configuration or dev scripts
+  locale | :globe_with_meridians: -> Made contributions to internationalization or localization
+  rls | :bookmark: -> release or version tag 
+  `
+  console.log(`\n[Error] Invalid commit message. A Correct COMMIT MESSAGE should be like below:`)
+  console.log('\x1b[33m%s', `\n    <type>(<scope>): <title><double breaks><body>`)
+  console.log(
+    '\x1b[37m',
+    `\nInternal supported types, and you can see more on https://gitmoji.dev/:`,
+  )
+  console.log('\x1b[33m%s', `\n${types}`)
+  console.log('\x1b[37m', '\nNice to code, bro!🦦')
+}
+
 // Get commit message from file
 const msgPath = resolve('./.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
+const mArr = msg.split('\n\n')
 
-const commitRegExp = /^(revert: )?(feat|perf|fix|docs|ref|style|chore|locale|rls)(\(.+\))?: .{1,50}/
+// body can be empty
+if (mArr.length > 2 || mArr.length === 0) {
+  genHelpInfo()
+  process.exit(1)
+}
 
-const helpInfo = `
-Error invalid commit message format.
-
-Proper commit message format is required for automated changelog generation. Examples:
-
-  feat(feature): 🔥 add new feature"
-  perf(performance): ⚡️ make some performance improvements'
-  fix: 🐛 fix bug
-  docs: 📖 add/update some docs
-  ref(refactor): 👾 make some refactor works
-  style: 🥳 better styles
-  chore(compiler): 🐙 Made some changes to the scaffolding
-  locale: 🌎 Made a small contribution to internationalization
-  rls(release): 🎉 Made a new release
-
-Have a great day and nice to code ~
-`
-
-if (!commitRegExp.test(msg)) {
-  console.error(helpInfo)
+const [title, body] = mArr
+const titleReg =
+  /^(feat|docs|style|fix|perf|ref|rm|break|conf|locale|rls|:sparkles:|:memo:|:lipstick:|:bug:|:zap:|:recycle:|:fire:|:boom:|:wrench:|:globe_with_meridians:|:bookmark:)(\(\w+\))?:\s*.{1,70}/
+const bodyReg = /^[A-Z0-9]{1}(.+)\.$/
+if (titleReg.test(title)) {
+  if (!!body && !bodyReg.test(body)) {
+    genHelpInfo()
+    process.exit(1)
+  }
+} else {
+  genHelpInfo()
   process.exit(1)
 }
